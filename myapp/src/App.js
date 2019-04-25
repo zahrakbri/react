84ec4857052b2212328a2c2f799b816d87928603
Zahra Kabiri <zahra@ravandyar.com>
2019-04-25 17:04:00 +0430

    Form

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,21 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <span>src/App.js</span> and save to reload.
-        </p>
-        <p> sdfgvhjnk </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React app rtfgyhuijkoldrfgtyhju
-        </a>
-      </header>
+      <div className='container'>
+        <img className='logo' />
+        <input className='my-input' placeholder='email' />
+        <input className='my-input' placeholder='password' />
+        <button className='submit'>
+          login
+        </button>
+      </div>
     </div>
   );
 }
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 var name = 'zahra'
@@ -13,16 +13,41 @@ const greeting =
   </div>
 
 function Welcome(props) {
+  console.log('props:::', props)
   return(
     <div>
-      <h1> salam zahra!</h1>
+      <h1> salam {props.firstname}     {props.lastname}!</h1>
     </div>
   )
 }
 
+function ParentComponent() {
+  return(
+    <div>
+      <Welcome firstname = 'rezvan' lastname = 'habibi' />
+      <Welcome name = 'ali' firstname = 'rezvan' lastname = 'habibi' />
+      <Welcome name = 'zahra' firstname = 'rezvan' lastname = 'habibi' />
+      <Welcome name = 'rezvan' firstname = 'rezvan' lastname = 'habibi' />
+    </div>
+  )
+}
+
+class ParentClass extends React.Component {
+  render() {
+    console.log('this::', this)
+    return(
+      <div>
+        <h1>
+          salam {this.props.name}!
+        </h1>
+      </div>
+    )
+  }
+}
 
 
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
