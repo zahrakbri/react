053cf58aa914f9cc5360df78e7fa384f681a0919
Zahra Kabiri <zahra@ravandyar.com>
2019-05-22 17:04:02 +0430

    fix merge conflict

--- myapp/src/reducer/conversation.js
+++ myapp/src/reducer/conversation.js
@@ -1,25 +1,7 @@
 import { stat } from "fs";
 
 const INIT = {
-<<<<<<< HEAD
   newMessage: ''
-=======
-  newMessage: '',
-  messages : [
-    {
-      id: 1,
-      message: 'salam'
-    },
-    {
-      id: 2,
-      message: 'salam'
-    },
-    {
-      id: 1,
-      message: 'khoobi?'
-    }
-  ]
->>>>>>> 26836e6df25319a9987923e4e00e6fa2140b22ea
 }
 
 function conversation(state = INIT, action) {
